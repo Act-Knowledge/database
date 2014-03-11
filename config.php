047be6f1126b8d60b76b86fbe5f739ae113de3df
jordynegen11 <jordy.visser@hotmail.com>
2014-03-11 13:45:56 +0100

    Database config verbeterd

--- config.php
+++ config.php
@@ -1,6 +1,16 @@
 <?php
+ 
+//Hostname, bijvoorbeeld: localhost of 127.0.0.1
+$host = "127.0.0.1";
+//Database gebruikersnaam
+$dbuser = "root";
+//Database wachtwoord
+$dbpass = "1234";
+//Database naam
+$dbname = "mark-it";
+
 //Database verbinding
-$con = mysqli_connect("127.0.0.1","root","1234","mark-it");
+$con = mysqli_connect($host,$dbuser,$dbpass,$dbname);
 
 
 //connectie error?
