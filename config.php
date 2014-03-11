ea57714d509cbcea08bba4865e637f1028752ad0
jordynegen11 <jordy.visser@hotmail.com>
2014-03-11 13:22:33 +0100

    Connectie verbeterd

--- config.php
+++ config.php
@@ -2,10 +2,11 @@
 //Database verbinding
 $con = mysqli_connect("127.0.0.1","root","1234","mark-it");
 
-if(!$con)
-{
-die("Fout in verbinding: ". mysqli_connect_error());
-}
 
+//connectie error?
+if (mysqli_connect_error($con))
+  {
+  die('Kan niet connecten: ' . mysqli_connect_error());
+  }
 
 ?>
