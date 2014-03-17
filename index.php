f2fc3c677990e88211eac18601cd3a430c03a1c8
nick076 <nickdekoning@ziggo.nl>
2014-03-17 13:36:28 +0100

    index.php

--- index.php
+++ index.php
@@ -1,42 +1,74 @@
-<?php
-
-require("./includes/config.php");
-
-?>
-
-<!doctype html><!-- Act-Knowledge "mark-it" 
-Laatste update: 2014-02-20 -->
-<html>
-<head>
+<html><head>
 	<meta charset="utf-8">
 	<!-- stylesheet -->
 	<title>Mark-it</title>
-	<link rel="stylesheet" href="css/style.css" />
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/style.css">
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet" type="text/css">
 	<link rel="icon" type="img/ico" href="img/web-icons/favicon.gif">
+
 </head>
 <body>	
 	<header class="top-fixed">
 		<nav class="menu">
 			<div class="top-left">
 				<span class="menu-icon">
-					<img class="top" src="img/icons/mnu-hover.png" />
-					<img class="bottom" src="img/icons/mnu.png" />
+					<img class="top" src="img/icons/mnu-hover.png">
+					<img class="bottom" src="img/icons/mnu.png">
 				</span>
+				
+				<!-- HTML for SEARCH BAR -->
+				<div id="tfheader">
+					<form id="tfnewsearch" method="get" action="http://www.google.com">
+					        <input type="text" class="tftextinput" name="q" size="21" maxlength="120"><input type="submit" value="search" class="tfbutton">
+					</form>
+				<div class="tfclear"></div>
+				</div>
+
 			</div><!-- end top-left -->
 
 			<div class="top-right">
 				<span class="optie-icon">
-					<img class="before" src="img/icons/opties-hover.png" />
-					<img class="after" src="img/icons/opties.png" />
+					<img class="before" src="img/icons/opties-hover.png">
+					<img class="after" src="img/icons/opties.png">
 				</span>
 			</div>
 		</nav>
 	</header><!-- end top-fixed -->
 
 	<div class="wrapper">
+		<div class="rij1">
+			<div class="tutorials">
+		 		<h2>titel</h2>
+		 		<section class="image"></section>
+		 		<section class="tekst">Lorem ipsum dolor sit amet, elit di consectetur adipiscing elit.</section>
+			</div>
+			<div class="tutorials">
+		 		<h2>titel</h2>
+			</div>
+		</div>
+		<div class="rij2">
+			<div class="fotos">
+		 		<h2>titel</h2>
+		 		<section class="image"></section>
+		 		<section class="tekst">Lorem ipsum dolor sit amet, elit di consectetur adipiscing elit.</section>
+			</div>
+			<div class="fotos">
+		 		<h2>titel</h2>
+			</div>
+		</div>
+		<div class="rij3">
+			<div class="videos">
+		 		<h2>titel</h2>
+		 		<section class="image"></section>
+		 		<section class="tekst">Lorem ipsum dolor sit amet, elit di consectetur adipiscing elit.</section>
+			</div>
+			<div class="videos">
+		 		<h2>titel</h2>
+			</div>
+		</div>
 	</div><!-- end wrapper -->
 
 	<footer></footer>
-</body>
-</html>
+
+
+</body></html>
