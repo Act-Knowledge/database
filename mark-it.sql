65f831e0d2fe4d92c61abbadf6230ebdd2e26230
jordynegen11 <jordy.visser@hotmail.com>
2014-03-17 11:32:06 +0100

    database update

--- mark-it.sql
+++ mark-it.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50141
 File Encoding         : 65001
 
-Date: 2014-03-17 09:55:25
+Date: 2014-03-17 11:31:47
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,9 +24,11 @@ CREATE TABLE `users` (
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
 INSERT INTO `users` VALUES ('1', 'jordy', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220');
+INSERT INTO `users` VALUES ('2', 'Glen', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220');
+INSERT INTO `users` VALUES ('3', 'Nick', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220');
