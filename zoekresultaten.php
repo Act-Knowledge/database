1400e27b3a903eb2c3052a3b445571d85c160d13
nick076 <nickdekoning@ziggo.nl>
2014-03-18 14:29:54 +0100

    menu en zoekresultaten

---
+++ zoekresultaten.php
@@ -0,0 +1,104 @@
+<html><head>
+	<meta charset="utf-8">
+	<!-- stylesheet -->
+	<title>Mark-it</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet" type="text/css">
+	<link rel="icon" type="img/ico" href="img/web-icons/favicon.gif">
+</head>
+<body>	
+	<header class="top-fixed">
+		<nav class="menu">
+			<div class="top-left">
+				<span class="menu-icon">
+					<ul id="menu">   
+   						 <li><img class="top" src="img/icons/mnu-hover.png">
+							 <img class="bottom" src="img/icons/mnu.png">
+
+       				 		<ul class="sub-menu">
+            					<li>
+                					<a href="index.php">Home</a>
+            					</li>
+            					<li>
+             					    <a href="#">tag</a>
+            					</li>
+           						<li>
+                					<a href="#">tag</a>
+            					</li>
+            					<li>
+                					<a href="#">tag</a>
+            					</li>
+        					</ul>
+    					</li>
+					</ul>
+				</span>
+			</div><!-- end top-left -->
+
+			<div class="top-right">
+				<span class="optie-icon">
+					<img class="before" src="img/icons/opties-hover.png">
+					<img class="after" src="img/icons/opties.png">
+				</span>
+			</div>
+		</nav>
+	</header><!-- end top-fixed -->
+
+	<div class="wrapper">
+		<div class="rij1-1">
+			<div class="tutorials">
+		 		<h3>Zoekresultaten</h3>
+		 		<section class="tekst">
+		 			<dl>
+		 				<dt><b>jQuery</b></dt>
+		 				<dd>Tag</dd>
+		 				<dd>Tag</dd>
+		 				<dd>Tag</dd>
+		 				<dd>Tag</dd>
+		 				<dd>Tag</dd>
+		 			</dl>
+		 			<br>
+		 			<dl>
+		 				<dt><b>PHP</b></dt>
+		 				<dd>Tag</dd>
+		 				<dd>Tag</dd>
+		 				<dd>Tag</dd>
+		 				<dd>Tag</dd>
+		 				<dd>Tag</dd>
+		 			</dl>
+		 			<br>
+		 			<dl>
+		 				<dt><b>HTML</b></dt>
+		 				<dd>Tag</dd>
+		 				<dd>Tag</dd>
+		 				<dd>Tag</dd>
+		 				<dd>Tag</dd>
+		 				<dd>Tag</dd>
+		 			</dl>
+		 			<br>
+		 			<dl>
+		 				<dt><b>CSS</b></dt>
+		 				<dd>Tag</dd>
+		 				<dd>Tag</dd>
+		 				<dd>Tag</dd>
+		 				<dd>Tag</dd>
+		 				<dd>Tag</dd>
+		 			</dl>
+		 		</section>
+			</div>
+		</div>
+		<div class="rij2-2">
+			<div class="fotos">
+		 		<h2>Titel</h2>
+		 		<section class="image"></section>
+		 		<section class="tekst">Lorem ipsum dolor sit amet, elit di consectetur adipiscing elit.</section>
+			</div>
+			<div class="fotos">
+		 		<h2>Titel</h2>
+			</div>
+		</div>
+	</div><!-- end wrapper -->
+
+	<footer></footer>
+
+
+</body></html>
