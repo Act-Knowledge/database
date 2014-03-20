4d28daee4f81e0f6a1c5702d3a36770030da1838
jordynegen11 <jordy.visser@hotmail.com>
2014-03-20 13:54:39 +0100

    Nieuwe Database

--- mark-it.sql
+++ mark-it.sql
@@ -10,10 +10,33 @@ Target Server Type    : MYSQL
 Target Server Version : 50141
 File Encoding         : 65001
 
-Date: 2014-03-17 11:31:47
+Date: 2014-03-20 13:54:30
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for tuts
+-- ----------------------------
+DROP TABLE IF EXISTS `tuts`;
+CREATE TABLE `tuts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titel` varchar(255) DEFAULT NULL,
+  `bericht` varchar(255) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `videolink` varchar(255) DEFAULT NULL,
+  `afbeelding` varchar(255) DEFAULT NULL,
+  `auteur` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of tuts
+-- ----------------------------
+INSERT INTO `tuts` VALUES ('1', 'Tut', 'dfdshufhdufhsdufhdshfudhsufhudsfus', '1', '', 'http://www.startpagina.nl/athene/dochters/php/images/php-logo.jpg', 'Jordy');
+INSERT INTO `tuts` VALUES ('2', 'Foto', 'dfdshufhdufhsdufhdshfudhsufhudsfus', '2', '', 'http://twittermania.nl/wp-content/uploads/2009/06/Afbeelding-12.png', 'Glen');
+INSERT INTO `tuts` VALUES ('3', 'Video', 'dfdshufhdufhsdufhdshfudhsufhudsfus', '3', '//www.youtube.com/embed/wn40ARXhas0', '', 'Nick');
+INSERT INTO `tuts` VALUES ('9', 'Jquery onzin!', 'alalalala jordy is de beste codeur ever!', '4', '', 'http://www.schoolplaten.com/hond-t20990.jpg', 'jordy');
 
 -- ----------------------------
 -- Table structure for users
